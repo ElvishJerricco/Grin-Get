db43e0a7daa03a8ca7e565cb189f91c4438ff338
ElvishJerricco <elvishjerricco@gmail.com>
2015-03-20 03:07:53 -0400

    Update Grin to fix unfortunately named directories

--- bin/grin-setup.lua
+++ bin/grin-setup.lua
@@ -1,8 +1,8 @@
 local grinDir = shell.resolve("/"..fs.combine(fs.getDir(shell.getRunningProgram()), ".."))
 
 local grinPackagePath = "/" .. fs.combine(grinDir, "packages/Team-CC-Corp/Grin/")
-local grinInstallPath = "/" .. fs.combine(grinPackagePath, "1.2.2")
-shell.run("pastebin", "run", "VuBNx3va", "-u", "Team-CC-Corp", "-r", "Grin", "-t", "1.2.2", grinInstallPath)
+local grinInstallPath = "/" .. fs.combine(grinPackagePath, "1.2.3")
+shell.run("pastebin", "run", "VuBNx3va", "-u", "Team-CC-Corp", "-r", "Grin", "-t", "1.2.3", grinInstallPath)
 local githubApiResponse = assert(http.get("https://api.github.com/repos/Team-CC-Corp/Grin/releases"))
 assert(githubApiResponse.getResponseCode() == 200, "Failed github response")
 local fh = fs.open(fs.combine(grinPackagePath, "releases.json"), "w")
